aaa256b1b5bb5f8e5a468610e08f44d4e2d89b5e
David Bochenski <david@bochenski.co.uk>
2011-04-28 16:44:42 +0100

    added support for landscape and lowdensity view

--- android/gen/com/gintellect/sudoku/R.java
+++ android/gen/com/gintellect/sudoku/R.java
@@ -10,14 +10,27 @@ package com.gintellect.sudoku;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
+    }
+    public static final class id {
+        public static final int about_button=0x7f060002;
+        public static final int continue_button=0x7f060000;
+        public static final int exit_button=0x7f060003;
+        public static final int new_button=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int about_label=0x7f050004;
+        public static final int app_name=0x7f050000;
+        public static final int continue_label=0x7f050002;
+        public static final int exit_label=0x7f050005;
+        public static final int main_title=0x7f050001;
+        public static final int new_game_label=0x7f050003;
     }
 }
