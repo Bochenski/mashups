10f527153b161c2e27650457a068164391def9a8
David Bochenski <david@bochenski.co.uk>
2011-05-01 17:58:25 +0100

    adding about dialog

--- android/src/com/gintellect/sudoku/Sudoku.java
+++ android/src/com/gintellect/sudoku/Sudoku.java
@@ -2,12 +2,38 @@ package com.gintellect.sudoku;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Sudoku extends Activity {
+public class Sudoku extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //set up click listeners for all the buttons
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+        
+    }
+    
+    public void onClick(View v){
+    	switch (v.getId()){
+    	case R.id.about_button:
+    	  Intent i = new Intent(this, About.class);
+    	  startActivity(i);
+    	  break;
+    	//More buttons go here (if any)
+    	}
     }
 }
