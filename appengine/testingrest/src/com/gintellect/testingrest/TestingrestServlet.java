36e28b043552c00f90c82807e940ab96ae521b94
unknown <David Bochenski@.360tech.local>
2011-05-03 16:58:07 +0100

    adding rest test app

---
+++ appengine/testingrest/src/com/gintellect/testingrest/TestingrestServlet.java
@@ -0,0 +1,13 @@
+package com.gintellect.testingrest;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TestingrestServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
