7ca47ca9ba1dc462cdf71dd561dea708a9a560ba
David Bochenski <david@bochenski.co.uk>
2012-09-02 18:03:09 +0100

    make first unit test pass!

--- node/chapter/test/unit/controllersSpec.js
+++ node/chapter/test/unit/controllersSpec.js
@@ -1,8 +1,23 @@
 describe('Book controllers', function() {
 	describe ('IndexCtrl', function() {
+
+		var scope, ctrl, $httpBackend;
+
+		beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+			$httpBackend = _$httpBackend_;
+			$httpBackend.expectGET('/api/books').
+				respond({"books": [ {title: "Book 1", chapters: "4"}, {title: "Book 2", chapters: "5"}]});
+			
+			scope = $rootScope.$new();
+			ctrl = $controller(IndexCtrl, {$scope: scope});
+		}));
+
 		it ('shoud display "books" model with 2 books', function() {
-			var scope = {},
-			ctrl = new IndexCtrl(scope);
+
+			expect(scope.books).toBeUndefined();
+			
+			$httpBackend.flush();
+
 			expect(scope.books.length).toBe(2);
 		});
 	});
