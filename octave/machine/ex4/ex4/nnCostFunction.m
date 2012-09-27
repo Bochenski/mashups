6a66fabc7d51871559486c998116f35ae50fbcf4
David Bochenski <david@bochenski.co.uk>
2012-09-27 14:26:29 +0100

    Completed EX4

--- octave/machine/ex4/ex4/nnCostFunction.m
+++ octave/machine/ex4/ex4/nnCostFunction.m
@@ -62,23 +62,80 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
+% calculate z of row 2 by multiplying by Theta1 transpose 
+Z2 = X * Theta1';
 
+% calculate sigmoid elementwise
+A2 = sigmoid(Z2);
 
+% Add ones to the A2 data matrix
+n = size(A2,1);
+A2 = [ones(n,1) A2];
 
+% forward propagate to the next level
+Z3 = A2 * Theta2';
+A3 = sigmoid(Z3);
 
+% A3 is now an m (number of examples) by n (number of categories) matrix (i.e. weve completed forward propagation)
 
+% need to craete Y with 1s in the locations of the relevant digits( zeros elsewhere ) so that it is 5000 x 10
+Y = zeros(m, num_labels);
 
+for i = 1 : num_labels
+	for j = 1 : m
+		if(y(j) == i)
+			Y(j,i) = 1;
+		endif
+	endfor
+endfor
 
+% crude non-vectorised implemnetation of the cost function, but I seem to recall this was deemed acceptable 
+% from the lecture videos
 
+for i = 1 : m
+	for k = 1  : num_labels
+		J = J + (  -Y(i,k) * log(A3(i,k)) - (1 - Y(i,k)) * log(1 -  A3(i,k)));
+	endfor
+endfor
 
+% next divide the sum by the number of training examples
+J = J/m;
 
+% step 2 - calculate the regularization part
+J = J + (sum(Theta1(:,2:end)(:).^2) + sum(Theta2(:,2:end)(:).^2)) * ( lambda / (2*m) );
 
+%forgot to take off the ones at the beginning of each matrix :-(
 
+for i = 1 : m
+	%sizeXi = size(X(i,:))
+	a1 = X(i, :);
+	Z2 = a1 * Theta1';
+	A2 = sigmoid(Z2);
+	n = size(A2,1);
+	A2 = [ones(n,1) A2];
+	Z3 = A2 * Theta2';
+	A3 = sigmoid(Z3);
+	d3 = A3 - Y(i,:);
+	d2 = (Theta2' * d3')(2:end, :) .* sigmoidGradient(Z2');
+	Theta2_grad = Theta2_grad .+ ( d3' * A2 );
+	Theta1_grad = Theta1_grad .+ ( d2 * a1 );
+endfor
 
+Theta2_grad = Theta2_grad ./ m;
+Theta1_grad = Theta1_grad ./ m;
 
+Theta2_reg = Theta2;
+Theta2_reg(:, 1) = zeros(size(Theta2_reg,1),1);
+Theta2_reg = Theta2_reg .* (lambda / m);
+Theta2_grad = Theta2_grad + Theta2_reg;
 
-
+Theta1_reg = Theta1;
+Theta1_reg(:,1) = zeros(size(Theta1_reg,1),1);
+Theta1_reg = Theta1_reg .* (lambda /m);
+Theta1_grad = Theta1_grad + Theta1_reg; 
 
 % -------------------------------------------------------------
 
