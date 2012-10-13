db969ee3a22bef820d2a89df42169a38f1e747bc
David Bochenski <david@bochenski.co.uk>
2012-10-13 17:06:49 +0100

    Completion of ex6 part 2 email spam detection

--- octave/machine/ex6/ex6/processEmail.m
+++ octave/machine/ex6/ex6/processEmail.m
@@ -97,14 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
+    for i = 1: size(vocabList,1)
+        if strcmp(str,vocabList{i})
+            word_indices = [word_indices; i];
+            break;
+        end
+    end
 
     % =============================================================
 
