cd3043ff55a84438f8c27caf7eec4808ffb096d0
David Bochenski <david@bochenski.co.uk>
2013-05-08 17:34:14 +0100

    Question 2

--- go/crypto/factoring.go
+++ go/crypto/factoring.go
@@ -7,6 +7,13 @@ import (
 )
 
 func main() {
+  fmt.Println("Question 1:")
+  q1()
+  fmt.Println("Question 2:")
+  q2()
+}
+
+func q1() {
   n := new(big.Int)
   sqrtN := new (big.Int)
   a := new(big.Int)
@@ -29,4 +36,33 @@ func main() {
 
   fmt.Println(a.Sub(a,x).String())
 
+}
+
+func q2() {
+  n := new(big.Int)
+  a := new(big.Int)
+  asquared := new(big.Int)
+  one := new(big.Int)
+  x := new(big.Int)
+  xsquared := new(big.Int)
+  p := new(big.Int)
+  q := new(big.Int)
+  candidate := new(big.Int)
+
+  n.SetString("648455842808071669662824265346772278726343720706976263060439070378797308618081116462714015276061417569195587321840254520655424906719892428844841839353281972988531310511738648965962582821502504990264452100885281673303711142296421027840289307657458645233683357077834689715838646088239640236866252211790085787877", 10)
+  one.SetString("1",10)
+
+  a = mathutil.SqrtBig(n)
+  for {
+    a.Add(a, one)
+    asquared.Mul(a,a)
+    xsquared.Sub(asquared,n)
+    x = mathutil.SqrtBig(xsquared)
+    p.Sub(a,x)
+    q.Add(a,x)
+    if candidate.Mul(p,q).Cmp(n) == 0 {
+      fmt.Println(p.String())
+      break
+    }
+  }
 }
